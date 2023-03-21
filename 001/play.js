aae2c217d64d92f8214c0be3d408ef8722d92dec
hyun-woo shin <S13200@poscoenc.com>
2023-03-21 09:33:34 +0900

    '

---
+++ 001/play.js
@@ -0,0 +1,11 @@
+const name='Max';
+let age = 19;
+const hasHabies = true;
+
+
+function summarizeUser(userName, userAge, userHasHobby){
+    return('Name is '+userName+' Age is '+ userAge+ ' Habby '+userHasHobby);
+}
+
+console.log(name,age,hasHabies);
+console.log(summarizeUser('shin', 45, true));
