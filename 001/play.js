a46ec77d2ebfe152bcef3b5dc59bb46fcb9c868e
hyun-woo shin <S13200@poscoenc.com>
2023-03-21 13:44:45 +0900

    .

--- 001/play.js
+++ 001/play.js
@@ -3,9 +3,29 @@ let age = 19;
 const hasHabies = true;
 
 
-function summarizeUser(userName, userAge, userHasHobby){
-    return('Name is '+userName+' Age is '+ userAge+ ' Habby '+userHasHobby);
+function summarizeUser1(userName, userAge, userHasHobby){
+    return ('Name is '+userName+' Age is '+ userAge+ ' Habby '+userHasHobby);
 }
 
+const summarizeUser2 = function(userName, userAge, userHasHobby){
+    return ('Name is '+userName+' Age is '+ userAge+ ' Habby '+userHasHobby);
+}
+
+const summarizeUser3 = (userName, userAge, userHasHobby) =>{
+    return ('Name is '+userName+' Age is '+ userAge+ ' Habby '+userHasHobby);
+}
+
+const summarizeUser4 = (userName, userAge, userHasHobby) => ('Name is '+userName+' Age is '+ userAge+ ' Habby '+userHasHobby);
+
+const add1 = (a,b)=>(a+b);
+const add2 = (a)=>(a+1);
+const add3= a=>a+1;
+
 console.log(name,age,hasHabies);
-console.log(summarizeUser('shin', 45, true));
+console.log(summarizeUser1('shin', 45, true));
+console.log(summarizeUser2('shin', 45, true));
+console.log(summarizeUser3('shin', 45, true));
+console.log(summarizeUser4('shin', 45, true));
+console.log(add1(1,2));
+console.log(add2(1));
+console.log(add3(1));
