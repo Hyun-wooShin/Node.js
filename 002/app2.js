69e34af07850bbd94e5676c6279f762aa07fc480
hyun-woo shin <S13200@poscoenc.com>
2023-03-23 11:23:32 +0900

    002 수정

---
+++ 002/app2.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/')
+    {
+        res.setHeader('Contet-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>test</title></head>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    
+    if(url === '/message' && method === 'POST')
+    {
+        const body = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Contet-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>test</title></head>');
+    res.write('<body><h1>test</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
