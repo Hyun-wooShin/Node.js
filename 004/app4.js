523b3903f2aeb09f89d234a399adb8f0ad75c555
hyun-woo shin <S13200@poscoenc.com>
2023-03-27 15:03:50 +0900

    004 수정

--- 004/app4.js
+++ 004/app4.js
@@ -1,17 +1,21 @@
 const express = require('express');
-
-const adminRoutes = require('./routes/admin');
-
-const shopRoutes = require('./routes/shop');
-
 const bodyParser = require("body-parser");
+
+//라우트 분리 사용
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+//라우트 분리 사용
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+//404 오류페이지 추가 마지막 미들웨어까지와서도 처리를 못하면 여기 미들웨어에서 에러를 반환함.
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>page not found</h1>');
+});
 
 app.listen(3000);
